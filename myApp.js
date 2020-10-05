5b23990bb94f63fcdeb8d00927a39ea0e2849190
ha trieu kim <hatrieukim@brickmate.kr>
2020-10-05 15:02:47 +0700

    send html file

--- myApp.js
+++ myApp.js
@@ -15,7 +15,9 @@ console.log("Hello World")
 
 
 /** 3) Serve an HTML file */
-
+app.get('/hereComeHtml', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
 
 /** 4) Serve static assets  */
 
